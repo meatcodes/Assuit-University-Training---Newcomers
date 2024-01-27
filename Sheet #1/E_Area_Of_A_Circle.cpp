9c62d854bf6fa5c5223c6d03af1631ea678c8abd
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-01-27 12:16:38 +0530

    Create E_Area_Of_A_Circle.cpp

---
+++ Sheet #1/E_Area_Of_A_Circle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double pi=3.141592653;
+    double a;
+    cin>>a;
+    cout<<setprecision(11)<<pi*(a*a)<<endl;
+    return 0;
+}
+
