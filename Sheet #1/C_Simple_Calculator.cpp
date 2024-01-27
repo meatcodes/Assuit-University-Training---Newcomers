ac5aa154be9fbd36c66253db394c2ab8a5f996c2
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-01-27 12:13:07 +0530

    Create C_Simple_Calculator.cpp

---
+++ Sheet #1/C_Simple_Calculator.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<cstring>
+using namespace std;
+int main()
+{
+    long long a,b;
+    cin>>a>>b;
+    cout<<a<<" "<<"+"<<" "<<b<<" "<<"="<<" "<<a+b<<endl;
+    cout<<a<<" "<<"*"<<" "<<b<<" "<<"="<<" "<<a*b<<endl;
+    cout<<a<<" "<<"-"<<" "<<b<<" "<<"="<<" "<<a-b<<endl;
+    return 0;
+}
+
