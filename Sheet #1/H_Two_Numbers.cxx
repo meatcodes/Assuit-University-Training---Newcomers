e50977fc53825c1cd6aa7e19374a8370aab93282
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-01-27 12:34:12 +0530

    Create H_Two_Numbers.cxx

---
+++ Sheet #1/H_Two_Numbers.cxx
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float n,m;
+    cin>>n>>m;
+    
+    cout<<"floor "<<n<<" / "<<m<<" "<<"="<<" "<<floor(n/m)<<"\n";
+    cout<<"ceil "<<n<<" / "<<m<<" "<<"="<<" "<<ceil(n/m)<<"\n";
+    cout<<"round "<<n<<" / "<<m<<" "<<"="<<" "<<round(n/m)<<"\n";
+
+    return 0;
+}
