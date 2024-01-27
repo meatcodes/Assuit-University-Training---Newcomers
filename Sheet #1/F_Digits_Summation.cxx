06296f5e997871b64a78a042390b9faebed85cb7
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-01-27 12:18:29 +0530

    Create F_Digits_Summation.cxx

---
+++ Sheet #1/F_Digits_Summation.cxx
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    long long a,b;
+    cin>>a>>b;
+    int c=a%10;
+    int d=b%10;
+    cout<<c+d<<endl;
+    return 0;
+}
