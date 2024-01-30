a59671d42869b9c7dcade422ea5c4a082589f626
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-01-30 10:35:04 +0530

    Create A_Add.cxx

---
+++ Sheet #5/A_Add.cxx
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void add(int a, int b)
+{
+     cout<<a+b;
+}
+
+int main()
+{
+     int a,b;
+     cin>>a>>b;
+     
+     add(a,b);
+     
+     return 0;
+}
