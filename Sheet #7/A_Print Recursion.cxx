216be9baf16986d96d35f1d8fb9bcb9b2998bb9c
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-02-21 09:51:37 +0530

    Create A_Print Recursion.cxx

---
+++ Sheet #7/A_Print Recursion.cxx
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+     if(n==0) return;
+     
+     cout<<"I love Recursion\n";
+     
+     print(n-1);
+}
+
+int main()
+{
+     int n;
+     cin>>n;
+     
+     print(n);
+     
+     return 0;
+}
