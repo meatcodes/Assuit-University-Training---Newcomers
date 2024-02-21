3e6817fb27f380bcecce542f511fe9a763268547
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-02-21 09:59:58 +0530

    Create C_Print from N to 1.cxx

---
+++ Sheet #7/C_Print from N to 1.cxx
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+     if(n==1) 
+     {
+          cout<<n<<"\n";
+          return;
+     }
+     
+     cout<<n<<" ";
+     
+     print(n-1);
+}
+
+int main()
+{
+     int n;
+     cin>>n;
+     
+     print(n);
+     
+     return 0;
+}
