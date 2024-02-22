75d504643c9343a8fe81e4569de5c71122935785
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-02-22 09:37:28 +0530

    Create H_Inverted Pyramid.cxx

---
+++ Sheet #7/H_Inverted Pyramid.cxx
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n, int row, int col, int spa)
+{
+     if(row==n) return;
+     
+     else 
+     {
+          if(spa<row)
+          {
+               cout<<" ";
+               print(n, row, col, spa+1);
+          }
+          else if(col<2*n-2*row-1)
+          {
+               cout<<"*";
+               print(n, row, col+1, spa);
+          }
+          else 
+          {
+               cout<<"\n";
+               print(n, row+1, 0, 0);
+          }
+     }
+}
+
+int main()
+{
+     int n;
+     cin>>n;
+     
+     print(n,0,0,0);
+     
+     return 0;
+}
