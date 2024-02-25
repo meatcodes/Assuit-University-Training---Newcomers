188c952340bd8695e1c55b4e86b204635eef5275
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-02-25 10:12:16 +0530

    Create D_Fixed_Password.cxx

---
+++ Sheet #2/D_Fixed_Password.cxx
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int a;
+     while(cin>>a)
+     {
+          if(a==1999) 
+          {
+               cout<<"Correct\n";
+               break;
+          }
+          else cout<<"Wrong\n";
+     }
+     
+     return 0;
+}
