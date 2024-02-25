e9edb2a28283b3d64acc8621469f32b9acb4993a
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-02-25 10:02:14 +0530

    Create B_Even_Numbers.cxx

---
+++ Sheet #2/B_Even_Numbers.cxx
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+     int n;
+     cin>>n;
+     
+     if(n==1) cout<<-1<<"\n";
+     else 
+     {
+          for(int i=2; i<=n; i+=2)
+          {
+               cout<<i<<"\n";
+          }
+     }
+     
+     return 0;
+}
