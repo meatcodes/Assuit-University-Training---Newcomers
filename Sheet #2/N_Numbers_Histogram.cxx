143a10d2697be7c4f49c2f7f4458ffebd658300b
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-02-25 11:07:25 +0530

    Create N_Numbers_Histogram.cxx

---
+++ Sheet #2/N_Numbers_Histogram.cxx
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     char a;
+     cin>>a;
+     
+     int n;
+     cin>>n;
+     
+     for(int i=0; i<n; i++) 
+     {
+          int s;
+          cin>>s;
+          
+          for(int i=0; i<s; i++) cout<<a;
+          cout<<"\n";
+     }
+     
+     return 0;
+}
