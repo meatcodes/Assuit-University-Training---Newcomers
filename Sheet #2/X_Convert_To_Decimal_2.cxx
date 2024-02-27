e17e7ce18fdbf71ae0fb033c0324b9e5eba5a0f3
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-02-27 10:02:00 +0530

    Create X_Convert_To_Decimal_2.cxx

---
+++ Sheet #2/X_Convert_To_Decimal_2.cxx
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int t;
+     cin>>t;
+     while(t--)
+     {
+          long long n;
+          cin>>n;
+          
+          int count=0;
+          while(n>0)
+          {
+               if(n%2==1) count++;
+               n=n/2;
+          }
+
+          
+          
+          long long ss=0;
+          
+          for(int i=0; i<count; i++)
+          {
+               ss=ss+pow(2,i);
+          }
+          
+          cout<<ss<<"\n";
+     }
+}
