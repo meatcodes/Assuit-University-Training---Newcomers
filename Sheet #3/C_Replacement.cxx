907a2854f66fbf2213d9b4b320a37776034dbe99
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-03-06 11:39:30 +0530

    Create C_Replacement.cxx

---
+++ Sheet #3/C_Replacement.cxx
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int n;
+     cin>>n;
+     
+     int arr[n];
+     for(int i=0; i<n; i++) cin>>arr[i];
+     
+     for(int i=0; i<n; i++)
+     {
+          if(arr[i]>0) arr[i]=1;
+          else if(arr[i]<0) arr[i]=2;
+     }
+     
+     for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+     cout<<"\n";
+     
+     return 0;
+}
