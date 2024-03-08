671a85c3a4607fdd5fc564e201853856eb4ed10f
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-03-08 13:33:10 +0530

    Create O_Fibonacci.cxx

---
+++ Sheet #3/O_Fibonacci.cxx
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int n;
+     cin>>n;
+     
+     long long arr[n];
+     arr[0]=0;
+     arr[1]=1;
+     
+     for(int i=2; i<=n; i++) arr[i]=arr[i-1]+arr[i-2];
+     
+     cout<<arr[n-1];
+     
+     return 0;
+}
