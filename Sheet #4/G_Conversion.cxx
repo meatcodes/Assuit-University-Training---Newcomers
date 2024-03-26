017d8db3f893e38eb4055f15dd01c80c25d7dea7
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-03-26 22:27:24 +0530

    Create G_Conversion.cxx

---
+++ Sheet #4/G_Conversion.cxx
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     string s;
+     cin>>s;
+     
+     for(int i=0; i<s.size(); i++)
+     {
+          if(s[i]>='a' && s[i]<='z') s[i]=char(s[i]-32);
+          else if(s[i]>='A' && s[i]<='Z') s[i]=char(s[i]+32);
+          else if(s[i]==',') s[i]=' ';
+     }
+     
+     cout<<s;
+     
+     return 0;
+}
