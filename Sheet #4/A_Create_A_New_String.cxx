cb15d215b77bae312bb8b2e8f23770698acf46f5
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-03-26 21:53:59 +0530

    Create A_Create_A_New_String.cxx

---
+++ Sheet #4/A_Create_A_New_String.cxx
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     string s,t;
+     cin>>s>>t;
+     
+     cout<<s.size()<<" "<<t.size()<<"\n";
+     cout<<s<<" "<<t<<"\n";
+     
+     return 0;
+}
